46d352c14b476ba816a30cd13973025be82f5ae8
Matt Comben <matthew@dockstudios.co.uk>
2020-03-11 18:06:34 +0000

    Rename TestShamean.cpp and update runs_scripts file accordingly

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,10 +7,10 @@ set -x
 g++ -fprofile-arcs -ftest-coverage -fPIC -O0 -g -I./ -c shamean.cpp -o shamean.o  -static
 
 # Build test file
-g++ -fprofile-arcs -ftest-coverage -fPIC -O0 -g -I./ -o testshamean TestShamean.cpp shamean.o -lcppunit -lcrypto
+g++ -fprofile-arcs -ftest-coverage -fPIC -O0 -g -I./ -o test_shamean test_shamean.cpp shamean.o -lcppunit -lcrypto
 
 # Run test executable
-./testshamean
+./test_shamean
 
 # Obtain coverage
 gcovr --xml -r . > coverage.xml
